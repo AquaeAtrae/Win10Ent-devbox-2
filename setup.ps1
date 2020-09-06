41aabef0bc8a152fb244ba7f8ebfbef429517559
AquaeAtrae <12989575+AquaeAtrae@users.noreply.github.com>
2020-09-06 13:49:19 -0600

    adding utils

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,7 @@
 'RUNNING: setup.ps1'
 'Installs Chocolatey, Boxstarter, and runs next script to build fresh Windows 10 machine.'
 ''
+# Uninterupted Operating System
 # by AquaeAtrae 8/30/2020
 
 
@@ -20,6 +21,12 @@
 #
 # Existing partitions labelled "Data" (D:) and "Files" (F:)
 # Existing user data F:\Users\<username>
+
+# RESOURCES:
+# Microsoft boxstarter sample scripts   https://github.com/microsoft/windows-dev-box-setup-scripts
+# Recommended dev tools https://www.hanselman.com/blog/ScottHanselmans2014UltimateDeveloperAndPowerUsersToolListForWindows.aspx
+# Sample boxstarter and good comments with extensive clean up of Windows https://gist.github.com/NickCraver/7ebf9efbfd0c3eab72e9
+
 
 $UserName = $env:username
 $UserDomain = $env:UserDomain
@@ -72,6 +79,7 @@ $Boxstarter.RebootOk=$true
 $Boxstarter.NoPassword=$false # Is this a machine with no logon password?
 $Boxstarter.AutoLogin=$true
 
+# SOURCE: https://github.com/microsoft/windows-dev-box-setup-scripts
 # Get the base URI path from the ScriptToCall value
 $bstrappackage = "-bootstrapPackage"
 $helperUri = $Boxstarter['ScriptToCall']
@@ -81,12 +89,12 @@ $helperUri = $helperUri.TrimStart("'", " ")
 $helperUri = $helperUri.TrimEnd("'", " ")
 $helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
 $helperUri += "/scripts"
-Write-Host "helper script base URI is $helperUri"
+write-host "helper script base URI is $helperUri"
 
-function ExecuteScript {
-  Param ([string]$script)
-  Write-Host "executing $helperUri/$script ..."
-	Invoke-Expression ((new-object net.webclient).DownloadString("$helperUri/$script"))
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
 }
 
 
@@ -225,6 +233,9 @@ if ($features.disable) {
 
 Enable-WindowsOptionalFeature -FeatureName $features.enable -Online -All -NoRestart
 
+# Enable long file names (beyond 260 chars)
+# [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem]
+#"LongPathsEnabled"=dword:00000001
 
 'DONE: Windows 10 Features enabled'
 ''
@@ -251,6 +262,9 @@ if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
   $file = "$env:TEMP\$item.appx"
   Write-Host "Downloading $item"
   curl.exe -sL https://aka.ms/$item -o $file
+	# TO DO: move appx installation to custom location
+	# https://kontext.tech/column/tools/308/how-to-install-windows-subsystem-for-linux-on-a-non-c-drive
+	
   Add-AppxPackage $file
   Remove-Item $file
 
@@ -270,7 +284,18 @@ if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
 	Ubuntu2004 run "printf ""%s:%s\n"" """ + $WSLCredential.GetNetworkCredential().UserName + """ """ + $WSLCredential.GetNetworkCredential().Password + """ | chpasswd"
 	
   Ubuntu2004 config --default-user $UserName
+	
+	choco install lxrunoffline
+
+	# icacls D:\wsl /grant "USERNAME:(OI)(CI)(F)"
+	# lxrunoffline move -n Ubuntu-20.04 -d D:\wsl\installed\Ubuntu-20.04  # relocate WSL root storage... but will it overwrite existing?!
+	lxrunoffline get-dir -n Ubuntu-20.04
+	
+	# mklink /D C:\project_directory \\wsl$\ubuntu-18.03\home\user\project_directory
 }
+
+choco install vcxsrv
+# choco install xming
 
 'DONE: WSL2 installed.'
 [console]::beep(500,300) # pitch, ms
@@ -281,15 +306,96 @@ if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
 
 'Installing software...'
 
+choco install webpi
+
+# .NET
+choco install dotnet4.5
+choco install dotnet4.6
+choco install dotnet4.6.1
+choco install dotnet4.6.2
+choco install dotnet4.7
+choco install dotnet4.7.1
+choco install dotnetcore-sdk
+choco install netfx-4.5.1-devpack
+choco install netfx-4.5.2-devpack
+choco install netfx-4.6.1-devpack
+choco install netfx-4.7-devpack
+choco install netfx-4.7.1-devpack
+choco install dotnetfx  # 4.8
+choco install netfx-4.8-devpack
+
+choco install manictime  # requires dotnet 4.5
+
+# %USERPROFILE%\.docker\daemon.json   # "data-root": "",
+choco install docker-desktop
+# enable resources | WSL integration?
+# choco install jq
+
+choco install  docker-compose
+choco install docker-kitematic
+
+# choco install virtualbox
+# choco install virtualbox-guest-additions-guest.install
+
 choco install windows-adk-all   # WinPE builder
 
-choco install pwsh `
-  microsoft-windows-terminal `
+choco install powershell-core
+choco install microsoft-windows-terminal
+# https://aka.ms/terminal-documentation
 
-cinst notepadplusplus
+choco install notepadplusplus
 
+choco install chocolateygui
+choco install boxstarter.hyperv
+choco install autoit.install
+choco install nircmd
+# choco install sudo
+choco install gsudo
+
+choco install sql-server-express -ia '/INSTALLSQLDATADIR=""F:\Data\MSSQL15.SQLEXPRESS""'
+# TO DO: modify nupkg (zip) adding $silentArgs for custom /INSTALLSQLDATADIR
+# https://chocolatey.org/api/v2/package/sql-server-express/2019.20200409
+choco install ssms
+
+# choco install vscode
+choco install vscodium
+choco pin add -n=vscodium
+choco install arduino
+choco install firacode
+choco install mkcert
+choco install bugshooting
+# Windows Steps Recorder is built-in
+
+# choco install intellijidea-community  # jetbrains java IDE
+# choco install phpstorm
+# choco install webstorm
+# choco install kubernetes-cli
+
+choco install winmerge	
+# choco install sublimetext2
+# atom
+	
+# choco install nxlog
+
+choco install everything  # search files and folders
 choco install sysinternals
+choco install handle  # shows which process has files open
+choco install powertoys
+choco install autoruns
 choco install linkshellextension
+choco install rufus
+choco install nirlauncher
+
+choco install crystaldiskinfo
+choco install crystaldiskmark
+choco install hwinfo.install
+choco install gpu-z
+choco install cpu-z
+choco install hwmonitor
+choco install windowsrepair
+
+choco install gpg4win
+choco install treesizefree
 
 
 #######################
@@ -297,31 +403,58 @@ choco install linkshellextension
 
 cinst googledrive 
 cinst google-drive-file-stream
+choco install google-backup-and-sync
+# choco install dropbox
+choco install nextcloud-client
+# choco install syncthing
+# choco install rsync
 
 cinst keepassxc
+choco install keepass-keepasshttp
 cinst authy-desktop
+# choco install lastpass
+choco install openvpn
+choco install nordvpn
+choco install veracrypt
 
 cinst openvpn
 
 choco install winscp 
-choco install putty.install 
+choco install curl
+choco install wget
+choco install putty
+choco install winscp
+# choco install utorrent
+choco install jq  # cli json processor
+choco install postman  # API testing tool
+choco install teracopy
+choco install grepwin
+# choco install logrotate
+
+# choco install wireshark
+# choco install winpcap
+# choco install nmap
+# choco install advanced-ip-scanner
+# choco install zap  # OWASP Zap proxy tester
+
 choco install zoom 
-#choco install logmein.client
+# choco install logmein.client
+# choco install teamviewer-qs
 
 choco install sharex
 
 choco install 7zip
 
-# choco install autohotkey  # FAILS
-
+choco install autohotkey  # FAILS?
+# choco install autohotkey.install
 
 choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /SChannel"'
 
-choco install dotnetcore-sdk `
-  adoptopenjdk11 `
+# choco install javaruntime-preventasktoolbar
+choco adoptopenjdk11 `
   adoptopenjdk8 `
-  gpg4win `
-  gsudo `
+
+choco install dotnetcore-sdk `
   hub `
   rapidee `
   slack `
@@ -329,9 +462,6 @@ choco install dotnetcore-sdk `
   gh `
   hub `
   7zip `
-  vscode `
-  docker-desktop `
-  docker-compose `
   googlechrome `
   firefox `
   nodejs `
@@ -342,6 +472,47 @@ choco install dotnetcore-sdk `
 #  apache-httpd `
 
 
+choco install microsoft-edge
+
+
+# choco install office365proplus
+# choco install libreoffice-fresh
+# choco install openoffice
+# choco install crystalreports2008runtime
+# choco install evernote
+# choco install adobe-creative-cloud
+
+# choco install qgis
+# choco install googleearthpro
+
+# choco install inkscape
+# choco install gimp
+# choco install drawio  # diagrams
+# choco install audacity
+# choco install audacity-lame  # mp3 encoder
+# choco install shotcut  # video editor
+# choco install openshot  # video editor
+# choco install avidemux
+# choco install carnac  # shows keystrokes for demos
+# choco install blender
+# choco install handbrake.install
+# choco install meshroom  # photoscanning
+
+# choco install discord.install
+# choco install steam
+# choco install origin
+# choco install uplay
+
+choco install dellcommandupdate
+choco install dell-update
+choco install geforce-experience
+# choco install msiafterburner
+choco install samsung-magician
+# choco install logitech-options
+# choco install ccleaner
+# choco install ccenhancer
+# choco install bulk-crap-uninstaller
+
 RefreshEnv
 
 
@@ -351,6 +522,11 @@ RefreshEnv
 #read-host "Press ENTER to continue or Ctrl-C to stop..."
 
 # 	[System.Environment]::SetEnvironmentVariable('TzHostDomainDone', $null, [System.EnvironmentVariableTarget]::Machine)  # clear variable
+
+choco install choco-package-list-backup
+# https://www.alexdresko.com/2014/12/22/automatically-generating-a-chocolatey-install-script/
+
+choco install choco-upgrade-all-at  # Task Scheduler to update all choco packages
 
 Enable-UAC
 Enable-MicrosoftUpdate
