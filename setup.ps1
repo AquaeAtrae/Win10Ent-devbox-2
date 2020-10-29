e8c08d0e4597992d43d956e43391e2079fd8e700
AquaeAtrae <12989575+AquaeAtrae@users.noreply.github.com>
2020-10-29 03:57:56 -0600

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -100,7 +100,7 @@ function executeScript {
 
 Disable-UAC
 Disable-MicrosoftUpdate
-
+bcdedit /timeout 5  # Set the default boot option time-out (seconds)
 
 #######################
 # Timezone, Hostname & Domain
@@ -112,6 +112,7 @@ $TzHostDomainDone = [System.Environment]::GetEnvironmentVariable("TzHostDomainDo
 if (-not $TzHostDomainDone) {
 
   $WSLUserName = $UserName.ToLower() -replace "[: ]", ""  # lowercase username stripped of invalid characters
+	# -replace " [$([RegEx]::Escape([string][IO.Path]::GetInvalidFileNameChars()))]+","_"
 
 	# TO DO: alternatively, just force password change after first login?
   [System.Management.Automation.PSCredential]$WSLCredential = $(Get-Credential -UserName $UserName -Message "Set WSL Linux username and initial password")
@@ -160,6 +161,7 @@ if (-not $TzHostDomainDone) {
 	}
 
 	[System.Environment]::SetEnvironmentVariable('TzHostDomainDone', $true, [System.EnvironmentVariableTarget]::Machine)
+	# Install-ChocolateyEnvironmentVariable "TzHostDomainDone" "$true" -VariableType Machine
 
 	'DONE: TimeZone, Computer Name, Domain.'
 	[console]::beep(500,300) # pitch, ms
@@ -180,58 +182,127 @@ Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
 'DONE: Remote access enabled.'
 [console]::beep(500,300) # pitch, ms
 
-'Enabling Windows 10 Features...'
-$features = @{
-  enable = @(
-		'Microsoft-Hyper-V-All',  # requires Windows 10 Professional or Enterprise
-		'Microsoft-Hyper-V',
-		'Microsoft-Hyper-V-Tools-All',
-		'Microsoft-Hyper-V-Management-PowerShell',
-		'Microsoft-Hyper-V-Hypervisor',
-		'Microsoft-Hyper-V-Services',
-		'Microsoft-Hyper-V-Management-Clients',
-		'HypervisorPlatform',    # requires Windows 10 Professional or Enterprise
-		
-    'Microsoft-Windows-Subsystem-Linux',
-    'Containers',
-		'Containers-DisposableClientVM',  # sandbox
-		'Windows-Defender-ApplicationGuard', # IE/Edge view untrusted websites from within VM sandbox
-		'TelnetClient ',
-		'TFTP',
-		
-		'SearchEngine-Client-Package',
-		'MSRDC-Infrastructure',
-		'WorkFolders-Client',
-		# 'Microsoft-Windows-NetFx4-US-OC-Package',
-		
-		'ServicesForNFS-ClientOnly',
-		'ClientForNFS-Infrastructure',
-		'NFS-Administration'
-  )
-  disable = @(
-    # 'Internet-Explorer-Optional-amd64'
-		# 'IIS-WebServerRole',
-		# 'MediaPlayback',
-		# 'WindowsMediaPlayer',
-		
-		# 'MSMQ-Container',  # Message queueing
-		# 'MSMQ-Server',
-		# 'HostGuardian',  # Guarded fabric and shielded VMs (cloud security), but blocks hibernation
-		
-		# 'NetFx3',
-		# 'Microsoft-Windows-NetFx3-OC-Package'
-  )
+function Assign-DriveLetter {
+    param(
+       [Parameter(Position=0,Mandatory)]
+       [string]$DesiredDriveLetter,
+
+       [Parameter(Position=1)]
+       [string]$DriveID,
+
+       [Parameter(Position=2)]
+       [string]$DriveSerial,
+
+       [Parameter(Position=3)]
+       [string]$DriveLabel
+       )
+
+    if (!$DriveID) {  if (!$DriveSerial) {  if (!$DriveLabel) {
+        $Drive = gwmi Win32_Volume | where {$_.Label -like $DriveLabel} | select -First 1
+    } else {
+        $Drive = gwmi Win32_Volume | where {$_.SerialNumber -like $DriveSerial} | select -First 1
+    } else {
+        $Drive = gwmi Win32_Volume | where {$_.DeviceID -like $DriveID} | select -First 1
+    } else {
+        'ERROR: Must identify the drive to assign letter via DriveID, DriveSerial, or DriveLabel'
+    }
+
+    if ($DesiredDriveLetter + ":" -ne $Drive.DriveLetter) {
+        $takenDriveLetters = (Get-PSDrive).Name -like '?'
+        if ($DesiredDriveLetter -in $takenDriveLetters) {
+            # Reassign existing drive to first unused drive letter to release the drive letter
+            $firstUnusedDriveLetter = [char[]] (0x44..0x5a) | where { $_ -notin $takenDriveLetters } | select -first 1  # ASCII letters D through Z
+            
+            Write-Host "Moving existing drive from $DesiredDriveLetter`: to $firstUnusedDriveLetter`:"
+            $ExistingDrive = gwmi Win32_Volume | where {$_.DriveLetter -like $DesiredDriveLetter + ":"}
+
+            if (!$ExistingDrive) {'Boxstarter reboot required'; Invoke-Reboot}  # boxstarter reboot
+            $ExistingDrive.AddMountPoint($firstUnusedDriveLetter + ":\")
+            $ExistingDrive.DriveLetter = $firstUnusedDriveLetter + ":"
+            $ExistingDrive.Put()
+            $ExistingDrive.Mount()
+        }
+
+        if ((Get-PSDrive).Name -like $DesiredDriveLette) {'Boxstarter reboot required'; Invoke-Reboot}  # boxstarter reboot
+
+        Write-Host "Assigning drive to $DesiredDriveLetter`:"
+        $Drive.AddMountPoint($DesiredDriveLetter + ":\")
+        $Drive.DriveLetter = $DesiredDriveLetter + ":"
+        $Drive.Put()
+        $Drive.Mount()
+    }
 }
+
+'Setting drive letter assignments'
+Assign-DriveLetter -DesiredDriveLetter "D" -DriveLabel "Data"
+Assign-DriveLetter -DesiredDriveLetter "F" -DriveLabel "Files"
+
+'Relocate User Folders'
+$home = "F:\Users\AquaeAtrae"
+Move-LibraryDirectory "Downloads" "$home\Downloads"
+Move-LibraryDirectory "Personal" "$home\Documents"
+Move-LibraryDirectory "Desktop" "$home\Desktop"
+Move-LibraryDirectory "My Video" "$home\Videos"
+Move-LibraryDirectory "My Pictures" "$home\Pictures"
+Move-LibraryDirectory "Favorites" "$home\Favorites"
+Move-LibraryDirectory "My Music" "$home\Music"
+# Move-LibraryDirectory "{56784854-C6CB-462B-8169-88E350ACB882}" "$home\Contacts"
+# Move-LibraryDirectory "{7D1D3A04-DEBB-4115-95CF-2F29DA2920DA}" "$home\Searches"
+# Move-LibraryDirectory "{BFB9D5E0-C6A9-404C-B2B2-AE6DB6AF4968}" "$home\Links"
+# Move-LibraryDirectory "{4C5C32FF-BB9D-43B0-B5B4-2D72E54EAAA4}" "$home\Saved Games"
+
+'Power Settings'
+powercfg -change -standby-timeout-ac 0
+powercfg -change -standby-timeout-dc 0
+powercfg -change -monitor-timeout-ac 20
+powercfg -change -monitor-timeout-dc 20
+powercfg -change -disk-timeout-ac 20
+powercfg -change -disk-timeout-dc 20
+powercfg -setacvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+powercfg -setdcvalueindex 381b4222-f694-41f0-9685-ff5bb260df2e 4f971e89-eebd-4455-a8de-9e59040e7347 5ca83367-6e45-459f-a27b-476b1d01c936 0
+powercfg -attributes SUB_SLEEP 9d7815a6-7ee4-497e-8888-515a05f02364 -ATTRIB_HIDE;  powercfg -h on     # not compatible with HostGuardian
+# Set-StartScreenOptions -EnableBootToDesktop -EnableDesktopBackgroundOnStart -EnableShowStartOnActiveScreen -EnableListDesktopAppsFirst
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtension
+Set-TaskbarOptions -Size Small -Dock Bottom -Combine Full -Lock
+Set-TaskbarOptions -Size Small -Dock Bottom -Combine Full -AlwaysShowIconsOn
+
+
 $winVer = [int](Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('ReleaseID')
 if ($winVer -ge 2004) {
   $features.enable += @('VirtualMachinePlatform')
 }
 
-if ($features.disable) {
-  Disable-WindowsOptionalFeature -FeatureName $features.disable -Online -NoRestart
-}
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-Tools-All -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-Management-PowerShell -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-Hypervisor -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-Services -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-Management-Clients -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName HypervisorPlatform -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Containers -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Containers-DisposableClientVM -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Windows-Defender-ApplicationGuard -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName TelnetClient -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName TFTP -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName SearchEngine-Client-Package -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName MSRDC-Infrastructure -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName WorkFolders-Client -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName ServicesForNFS-ClientOnly -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName ClientForNFS-Infrastructure -Online -All -NoRestart
+Enable-WindowsOptionalFeature -FeatureName NFS-Administration -Online -All -NoRestart
+Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName IIS-WebServerRole -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName MediaPlayback -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName WindowsMediaPlayer -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName MSMQ-Container -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName MSMQ-Server -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName HostGuardian -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName NetFx3 -Online -NoRestart
+Disable-WindowsOptionalFeature -FeatureName Microsoft-Windows-NetFx3-OC-Package -Online -NoRestart
+Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-NetFx4-US-OC-Package -Online -All -NoRestart
 
-Enable-WindowsOptionalFeature -FeatureName $features.enable -Online -All -NoRestart
 
 # Enable long file names (beyond 260 chars)
 # [HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\FileSystem]
@@ -249,7 +320,7 @@ Enable-WindowsOptionalFeature -FeatureName $features.enable -Online -All -NoRest
 
 
 # Start-Process msiexec.exe -Wait -ArgumentList '/I https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi /quiet /qn /norestart'
-Start-Process msiexec.exe -Wait -ArgumentList '/I https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi /passive'
+Start-Process msiexec.exe -Wait -ArgumentList '/I https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi /passive'  # REPEATS ON REBOOT UNNECESSARILY
 
 $winVer = [int](Get-Item "HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion").GetValue('ReleaseID')
 if ($winVer -ge 2004) {
@@ -272,6 +343,9 @@ if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
 
   Ubuntu2004 install --root
   Ubuntu2004 config --default-user root
+	
+	# mklink /D C:\project_directory \\wsl$\ubuntu-18.03\home\USER\project_directory
+	
   Ubuntu2004 run "curl -sL '$helperUri/WSL.sh' | bash"
 	# Ubuntu2004 run "curl -sL '$helperUri/WSL.sh' | sed -En ""s/USERNAME/$UserName/g"" | bash"
   
@@ -291,7 +365,6 @@ if (!(Get-Command "ubuntu2004.exe" -ErrorAction SilentlyContinue)) {
 	# lxrunoffline move -n Ubuntu-20.04 -d D:\wsl\installed\Ubuntu-20.04  # relocate WSL root storage... but will it overwrite existing?!
 	lxrunoffline get-dir -n Ubuntu-20.04
 	
-	# mklink /D C:\project_directory \\wsl$\ubuntu-18.03\home\user\project_directory
 }
 
 choco install vcxsrv
@@ -301,12 +374,21 @@ choco install vcxsrv
 [console]::beep(500,300) # pitch, ms
 #read-host "Press ENTER to continue or Ctrl-C to stop..."
 
+
+#######################
+# Profile
+
+# OLT Photoshop Macros.atn
+# G Hub macros
+# Lightroom Catalogs
+
+
 #######################
 # Installing software
 
 'Installing software...'
 
-choco install webpi
+# choco install webpi
 
 # .NET
 choco install dotnet4.5
@@ -315,13 +397,15 @@ choco install dotnet4.6.1
 choco install dotnet4.6.2
 choco install dotnet4.7
 choco install dotnet4.7.1
+choco install dotnet4.7.2
+choco install dotnetfx  # 4.8
 choco install dotnetcore-sdk
 choco install netfx-4.5.1-devpack
 choco install netfx-4.5.2-devpack
 choco install netfx-4.6.1-devpack
 choco install netfx-4.7-devpack
-choco install netfx-4.7.1-devpack
-choco install dotnetfx  # 4.8
+#choco install netfx-4.7.1-devpack  # FAILS
+choco install netfx-4.7.2-devpack
 choco install netfx-4.8-devpack
 
 choco install manictime  # requires dotnet 4.5
@@ -331,7 +415,7 @@ choco install docker-desktop
 # enable resources | WSL integration?
 # choco install jq
 
-choco install  docker-compose
+choco install docker-compose
 choco install docker-kitematic
 
 # choco install virtualbox
@@ -347,13 +431,14 @@ choco install notepadplusplus
 
 choco install chocolateygui
 choco install boxstarter.hyperv
+choco install cloneapp	# backup and recover app configs and registries
+# cloneapp-ua 
+
 choco install autoit.install
-choco install nircmd
 # choco install sudo
 choco install gsudo
 
 choco install sql-server-express -ia '/INSTALLSQLDATADIR=""F:\Data\MSSQL15.SQLEXPRESS""'
-# TO DO: modify nupkg (zip) adding $silentArgs for custom /INSTALLSQLDATADIR
 # https://chocolatey.org/api/v2/package/sql-server-express/2019.20200409
 choco install ssms
 
@@ -384,7 +469,8 @@ choco install powertoys
 choco install autoruns
 choco install linkshellextension
 choco install rufus
-choco install nirlauncher
+choco install nirlauncher  # includes Win32/Unwaders adware?!
+choco install nircmd
 
 choco install crystaldiskinfo
 choco install crystaldiskmark
@@ -392,7 +478,8 @@ choco install hwinfo.install
 choco install gpu-z
 choco install cpu-z
 choco install hwmonitor
-choco install windowsrepair
+
+choco install windowsrepair  # tweaking.com
 
 choco install gpg4win
 choco install treesizefree
@@ -401,29 +488,28 @@ choco install treesizefree
 #######################
 # NextCloud, Google Drive, KeePass, Authy, Putty, winscp, OpenVPN, SSH Key Agent, notepad++, 7zip
 
-cinst googledrive 
-cinst google-drive-file-stream
+choco install googledrive 
+choco install google-drive-file-stream
 choco install google-backup-and-sync
 # choco install dropbox
 choco install nextcloud-client
 # choco install syncthing
 # choco install rsync
 
-cinst keepassxc
+choco install keepassxc
 choco install keepass-keepasshttp
-cinst authy-desktop
+choco install authy-desktop
 # choco install lastpass
 choco install openvpn
-choco install nordvpn
+# choco install nordvpn  # FAILS
 choco install veracrypt
 
-cinst openvpn
+choco install openvpn
 
 choco install winscp 
 choco install curl
 choco install wget
 choco install putty
-choco install winscp
 # choco install utorrent
 choco install jq  # cli json processor
 choco install postman  # API testing tool
@@ -444,8 +530,10 @@ choco install zoom
 choco install sharex
 
 choco install 7zip
+Install-Module -Name 7Zip4Powershell
+Import-Module -Name 7Zip4Powershell -Global
 
-choco install autohotkey  # FAILS?
+choco install autohotkey
 # choco install autohotkey.install
 
 choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration /SChannel"'
@@ -454,20 +542,17 @@ choco install git -params '"/GitAndUnixToolsOnPath /WindowsTerminal /NoShellInte
 choco adoptopenjdk11 `
   adoptopenjdk8 `
 
-choco install dotnetcore-sdk `
-  hub `
+choco install hub `
   rapidee `
   slack `
-  keybase `
+  keybase `  # requires dotnet4.7
   gh `
   hub `
-  7zip `
   googlechrome `
-  firefox `
+  firefox `  # FAILS
   nodejs `
-  putty `
   maven `
-#	jetbrainstoolbox `
+#  jetbrainstoolbox `
 #  bitnami-xampp `
 #  apache-httpd `
 
@@ -475,12 +560,13 @@ choco install dotnetcore-sdk `
 choco install microsoft-edge
 
 
+choco install microsoft-office-deployment --params="'/64bit /Product:ProPlus2019Volume /Exclude:OneDrive,Outlook,Lync,Groove'"  # Office Volume license requires MLK key
 # choco install office365proplus
 # choco install libreoffice-fresh
 # choco install openoffice
 # choco install crystalreports2008runtime
 # choco install evernote
-# choco install adobe-creative-cloud
+# choco install adobe-creative-cloud  # FAILS error code 72
 
 # choco install qgis
 # choco install googleearthpro
@@ -505,7 +591,8 @@ choco install microsoft-edge
 
 choco install dellcommandupdate
 choco install dell-update
-choco install geforce-experience
+choco install nvidia-display-driver
+# choco install geforce-experience  # FAILS
 # choco install msiafterburner
 choco install samsung-magician
 # choco install logitech-options
@@ -514,6 +601,9 @@ choco install samsung-magician
 # choco install bulk-crap-uninstaller
 
 RefreshEnv
+
+$chrome = convert-path (ls "$env:localappdata\" -recurse -include "chrome.exe")[0].pspath
+If ($chrome -ne "")	{	Install-ChocolateyPinnedTaskBarItem $chrome }
 
 
 
